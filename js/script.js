3d0fd77db1f581dfa2f335b3b124c4239be110de
DhanawatS <shubhrit.dhanawat@cognizant.com>
2019-12-03 15:26:39 +0530

    Added script

---
+++ js/script.js
@@ -0,0 +1,41 @@
+
+var videoElement = document.querySelector('video');
+
+getStream().then(getDevices).then(gotDevices);
+
+function getDevices() {
+    // AFAICT in Safari this only gets default devices until gUM is called :/
+    return navigator.mediaDevices.enumerateDevices();
+}
+
+function gotDevices(deviceInfos) {
+    window.deviceInfos = deviceInfos; // make available to console
+    console.log('Available input and output devices:', deviceInfos);
+
+}
+
+function getStream() {
+    if (window.stream) {
+        window.stream.getTracks().forEach(track => {
+            track.stop();
+        });
+    }
+
+    const constraints = {
+        audio: false,
+        video: {
+            facingMode: 'environment'
+        }
+    };
+    return navigator.mediaDevices.getUserMedia(constraints).
+        then(gotStream).catch(handleError);
+}
+
+function gotStream(stream) {
+    videoElement.srcObject = stream;
+    document.getElementById('sky').setAttribute('src', '#ground');
+}
+
+function handleError(error) {
+    console.error('Error: ', error);
+}
